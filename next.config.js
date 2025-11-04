5f72c1c7d4417ba8bc9a6079c7b28bc8642fa4e4
deadmarket <dead.markett@gmail.com>
2025-11-03 23:03:34 -0300

    redirect www -> deadmarket.com.ar in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }] }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.deadmarket.com.ar' }],
+        destination: 'https://deadmarket.com.ar/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
